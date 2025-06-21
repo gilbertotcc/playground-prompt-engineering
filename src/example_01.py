088fa485253424bb771bb0430e55850953ac1e43
Gilberto Taccari <gilberto.taccari@gmail.com>
2025-06-22 00:41:59 +0200

    Test with ollama

--- src/example_01.py
+++ src/example_01.py
@@ -4,6 +4,8 @@ from langchain_core.messages import SystemMessage, HumanMessage
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_community.tools import ShellTool
 
+# from langchain_ollama.llms import OllamaLLM
+
 # Load environment variables from .env file
 load_dotenv()
 
@@ -13,6 +15,8 @@ llm = ChatGoogleGenerativeAI(
     model="gemini-2.0-flash",
     temperature=0,
 )
+
+# llm = OllamaLLM(model="deepseek-r1")
 
 prompt = """ Q: Mrs. Smith currently has 10 students in her class.
 She receives 3 new groups of students over the week. Each group contains 4 students.
@@ -25,6 +29,10 @@ messages = [SystemMessage(content=prompt), HumanMessage(content=prompt)]
 response = llm.invoke(messages)
 
 output = response.content
+metadata = response.usage_metadata
 
 print("--------------Response from the model--------------------")
 print(output)
+
+print("--------------Response from the model--------------------")
+print(metadata)
