00200d9794ff07db3291d3509c217a9e8e03ea04
Gilberto Taccari <gilberto.taccari@gmail.com>
2025-06-21 19:53:25 +0200

    Add example_01.py

---
+++ src/example_01.py
@@ -0,0 +1,30 @@
+from dotenv import load_dotenv
+from langchain_core.messages import SystemMessage, HumanMessage
+
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_community.tools import ShellTool
+
+# Load environment variables from .env file
+load_dotenv()
+
+shell_tool = ShellTool()
+
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.0-flash",
+    temperature=0,
+)
+
+prompt = """ Q: Mrs. Smith currently has 10 students in her class.
+She receives 3 new groups of students over the week. Each group contains 4 students.
+How many students does Mrs. Smith have in her class now?
+A: Let's think step by step
+"""
+
+messages = [SystemMessage(content=prompt), HumanMessage(content=prompt)]
+
+response = llm.invoke(messages)
+
+output = response.content
+
+print("--------------Response from the model--------------------")
+print(output)
